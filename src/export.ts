2941aa71158a3b0cf773b952e1f6cd25ebd10333
Mark van Wyk <mark@flyh2.com>
2021-05-20 13:33:55 +0200

    Trying export

--- src/export.ts
+++ src/export.ts
@@ -1,17 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
 import WindflowMagicToolbar from './components/WindflowMagicToolbar.vue'
 import WindflowApp from './components/WindflowApp.vue'
 import WindflowArea from './components/WindflowArea.vue'
 import WindflowLayout from './components/WindflowLayout.vue'
 
-createApp(App).mount('#app')
-
-export default {
-    components: {
-        WindflowMagicToolbar: WindflowMagicToolbar,
-        WindflowApp: WindflowApp,
-        WindflowArea: WindflowArea,
-        WindflowLayout: WindflowLayout,
-    }
-}
+export {WindflowMagicToolbar, WindflowApp, WindflowArea, WindflowLayout}
